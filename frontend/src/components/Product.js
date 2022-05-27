0a47a523606f0efd99bf92fadbf2b4441ec983d2
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-05-27 16:53:41 +0600

    product deatils done

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,109 +1,30 @@
-
-import React from "react";
-import { Panel, Rate } from "rsuite";
-import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+import React, { useState } from "react";
+import { Panel } from "rsuite";
 import { AiOutlineShopping } from "react-icons/ai";
+import Rating from "./Rating";
 
 function Product(props) {
+  const [activeColor, setActiveColor] = useState("red");
+
   return (
     <div className="single-product">
       <Panel bodyFill style={{ display: "inline-block", width: "100%" }}>
         <img src={props.img} style={{ width: "100%" }} alt={props.name} />
 
         <div className="product-box">
-          <div className="product-icon">
-            {props.rating >= 1 ? (
-              <BsStarFill className="starIcon" />
-            ) : props.rating >= 0.5 ? (
-              <BsStarHalf className="starIcon" />
-            ) : (
-              <BsStar className="starIcon" />
-            )}
-            {props.rating >= 2 ? (
-              <BsStarFill className="starIcon" />
-            ) : props.rating >= 1.5 ? (
-              <BsStarHalf className="starIcon" />
-            ) : (
-              <BsStar className="starIcon" />
-            )}
-            {props.rating >= 3 ? (
-              <BsStarFill className="starIcon" />
-            ) : props.rating >= 2.5 ? (
-              <BsStarHalf className="starIcon" />
-            ) : (
-              <BsStar className="starIcon" />
-            )}
-            {props.rating >= 4 ? (
-              <BsStarFill className="starIcon" />
-            ) : props.rating >= 3.5 ? (
-              <BsStarHalf className="starIcon" />
-            ) : (
-              <BsStar className="starIcon" />
-            )}
-            {props.rating >= 5 ? (
-              <BsStarFill className="starIcon" />
-            ) : props.rating >= 4.5 ? (
-              <BsStarHalf className="starIcon" />
-            ) : (
-              <BsStar className="starIcon" />
-            )}
-          </div>
+          <Rating rating={props.rating} />
           <div className="brand">
             <p className="productBrand"> {props.brand}</p>
           </div>
         </div>
-
-import React, { useState } from 'react'
-import { Panel } from 'rsuite'
-import { AiOutlineShopping } from 'react-icons/ai';
-import Rating from './Rating';
-
-function Product(props) {
-
-    console.log(props)
-
-    const [activeColor, setActiveColor] = useState("red")
-
-
-    return (
-        <div className='single-product'>
-            <Panel bodyFill style={{ display: 'inline-block', width: "100%" }}>
-                <img src={props.img} style={{ width: "100%" }} alt={props.name} />
-
-
-
-                <div className='product-box'>
-                    <Rating rating={props.rating} />
-                    <div className="brand">
-                        <p className='productBrand'> {props.brand}</p>
-                    </div>
-                </div>
-                <Panel header={props.heading}>
-                    <div className='product-box'>
-                        <div className="product-color-box">
-                            {props.color.map(item => (
-                                <span className={activeColor == item ? 'productColor activeColor' : 'productColor'} style={{ background: `#${item}` }}></span>
-                            ))}
-                        </div>
-                        <div className="product-size">
-                            {props.size.map(item => (
-                                <span>{item}</span>
-                            ))}
-                        </div>
-                    </div>
-                    <span className='cart-product'>
-                        <AiOutlineShopping className='shopping-bag' />
-                    </span>
-                    <span className='price'>
-                        ${props.price}
-                    </span>
-
-
         <Panel header={props.heading}>
           <div className="product-box">
             <div className="product-color-box">
               {props.color.map((item) => (
-                <span className="productColor" style={{ background: `#${item}` }}></span>
+                <span
+                  className={activeColor == item ? "productColor activeColor" : "productColor"}
+                  style={{ background: `#${item}` }}
+                ></span>
               ))}
             </div>
             <div className="product-size">
@@ -112,13 +33,10 @@ function Product(props) {
               ))}
             </div>
           </div>
-
           <span className="cart-product">
             <AiOutlineShopping className="shopping-bag" />
           </span>
-
           <span className="price">${props.price}</span>
-
           {/* {props.color.map(item => (
                         <span className='productColor' style={{ background: `#${item}` }}></span>
                     ))} */}
