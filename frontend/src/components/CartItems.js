4eb027c2a74d4089ed8a9342aae6aa7a9c50ccbc
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-06-03 02:21:00 +0600

    problem fix

--- frontend/src/components/CartItems.js
+++ frontend/src/components/CartItems.js
@@ -168,7 +168,22 @@ const CartItems = () => {
                 <div className="recent_viewed_title">
                   <h1>RECENTLY VIEWED</h1>
                 </div>
-                <div className="recent_viewed_card">
+                {state.cart.cartItems.map((item, releted) => (
+                  <Col key={releted} xs={8}>
+                    <Product
+                      img={item.imageUrls}
+                      heading={item.title}
+                      review={item.review}
+                      brand={item.brand}
+                      color={item.color}
+                      size={item.sizes}
+                      price={item.price}
+                      item={item}
+                      slug={item.slug}
+                    />
+                  </Col>
+                ))}
+                {/* <div className="recent_viewed_card">
                   <Grid>
                     <Row className="show-grid" gutter={30}>
                       {recentViewed.map((item) => (
@@ -186,7 +201,7 @@ const CartItems = () => {
                       ))}
                     </Row>
                   </Grid>
-                </div>
+                </div> */}
               </Col>
             </Row>
           </Grid>
